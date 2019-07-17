68e2d8ea1cf2f997ebe5cd1f4cf1c6dab13ddcc8
Paul Kigozi <pwndz172@gmail.com>
2019-07-17 10:35:04 +0100

    Refactor move forward and backward to share logic

--- src/main/java/marsrover/Point.java
+++ src/main/java/marsrover/Point.java
@@ -3,49 +3,47 @@ package marsrover;
 public class Point {
 	private int value;
 	private int valueMax;
-	
-	
+
 	public Point(int value) {
 		super();
 		this.value = value;
 	}
-	
+
 	public Point(int value, int valueMax) {
 		super();
 		this.value = value;
 		this.valueMax = valueMax;
 	}
-	
+
 	public int getValueMax() {
 		return valueMax;
 	}
-	
-	
+
 	public int getValue() {
 		return value;
 	}
+
 	public void setValue(int value) {
 		this.value = value;
 	}
 
-
-
 	public void moveForward() {
-		value = value + 1;
-		if( value > valueMax ) {
-			value = 0;
-		}
-		
+		move(1);
+
 	}
-	
-	public void moveBackward() {
-		value = value - 1;
-		if( value < 0 ) {
+
+	public void move(int unit) {
+		value = value + unit;
+		if (value > valueMax) {
+			value = 0;
+		} else if (value < 0) {
 			value = valueMax;
 		}
-		
+
 	}
-	
-	
-	
+
+	public void moveBackward() {
+		move(-1);
+	}
+
 }
